60bfeaeae54bc42d078056f1c0e3cbc415290543
Igor Paslauski Pedroso de Oliveira <io006235@cfjl.com.br>
2025-05-04 11:20:27 -0300

    ajuste no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,8 @@
-
-import React from 'react';
+import React from "react";
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
-  
+
   return (
     <footer className="bg-purple-darker text-white py-10">
       <div className="container mx-auto px-6">
@@ -12,10 +11,13 @@ const Footer: React.FC = () => {
             <h3 className="text-xl font-bold">Eduarda Siqueira</h3>
             <p className="text-gray-300 mt-1">Desenvolvedora Front-end</p>
           </div>
-          
           <div className="text-center md:text-right">
-            <p className="text-gray-300">© {currentYear} Todos os direitos reservados</p>
-            <p className="text-sm text-gray-400 mt-1">Desenvolvido com React & Tailwind CSS</p>
+            <p className="text-gray-300">
+              © {currentYear} Todos os direitos reservados
+            </p>
+            <p className="text-sm text-gray-400 mt-1">
+              Desenvolvido por Ígor Paslauski Pedroso de Oliveira
+            </p>
           </div>
         </div>
       </div>
